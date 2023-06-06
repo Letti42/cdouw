b052aeb10445a7f0d44bb1842b1f849ae987a98a
Letti42 <78980794+Letti42@users.noreply.github.com>
2023-06-05 21:38:13 -0700

    sign in and token gen

--- index.js
+++ index.js
@@ -13,11 +13,13 @@ cors_proxy.createServer({
 });*/
 
 
-var util = require('util');
-var exec = require('child_process').exec;
+import { exec } from 'child_process';
+import queryString from "query-string";
+import { config } from 'dotenv'; config();
+import { getAuthToken } from './tokengen.js';
 
-var command = 'curl -H "Origin: https://example.reqbin.com" http://localhost:5000'
-
+var command = 'curl -H "Origin: https://studio.code.org/" https://studio.code.org/api/v1/projects/gallery/public/all/100'
+/*
 child = exec(command, function(error, stdout, stderr){
 
 console.log('stdout: ' + stdout);
@@ -29,3 +31,29 @@ if(error !== null)
 }
 
 });
+*/
+function loginReq(token) {
+    let cred = { "user[hashed_email]": process.env.EMAIL, "user[login]": "", "user[password]": process.env.PASSWORD, "authenticity_token": token };
+    let form = queryString.stringify(cred);
+
+    let cmd = `curl -v -X POST https://studio.code.org/users/sign_in -H "Origin: https://studio.code.org/users/sign_in" -H "Content-Type: application/x-www-form-urlencoded" -d "${form}"`
+
+    exec(cmd, function (error, stdout, stderr) {
+
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+
+        if (error !== null) {
+            console.log('exec error: ' + error);
+        }
+
+    });
+}
+
+//
+
+;(async()=>{
+    let token = await getAuthToken();
+    console.log(token);
+    //loginReq(token);
+})();
